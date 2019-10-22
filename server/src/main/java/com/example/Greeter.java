9d2f01f217fa81db870b882dd34f4b567968291f
mozodan <mozodan@yahoo.com>
2019-10-22 12:08:13 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -4,6 +4,8 @@ package com.example;
  * This is a class.
  */
 public class Greeter {
+  
+  public Long MAGICO = new Long("2333");
 
   /**
    * This is a constructor.
@@ -16,11 +18,11 @@ public class Greeter {
    * Metodo de greet modificado.
    */
   public String greet(String someone) {
-    Long lalala = new Long("2333");
-    if (lalala == 2333) {
-      return String.format("Hello Danield, %s!", someone); }
-    else{ 
+    //Long lalala = new Long("2333");
+    if(MAGICO > 1){
       return String.format("Hello, %s!", someone);
+    }else{
+        return String.format("Hello, %s!", someone);
     }
   }
 }
